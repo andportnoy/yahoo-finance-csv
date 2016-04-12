15a5d1402d8239e7e0455ad10277ad22fdf4b07b
Andrey Portnoy <aportnoy@ucsd.edu>
2016-04-12 00:04:51 -0700

    pandas handling moved to data ops file

--- user_operations.py
+++ user_operations.py
@@ -24,7 +24,6 @@ def current(tickers, write_to_csv=False, result_csv_path=None, api_dict_csv_path
     param_list = dops.get_param_list_from_api_dict(api_dict)
     param_string = dops.get_param_string_from_list(param_list)
 
-    # create ticker string for the request
 
     try:
         if type(tickers) == str:
@@ -43,26 +42,10 @@ def current(tickers, write_to_csv=False, result_csv_path=None, api_dict_csv_path
     answer_string = dops.get_answer_string(ticker_string, param_string)
     answer_list = dops.get_answer_list_from_string(answer_string)
 
-    # lookup the parameter definitions and create a dictionary to be transformed into a pandas DataFrame
-    dict_for_pandas = {api_dict[param_list[index]]: item for index, item in enumerate(zip(*answer_list))}
-    pandas_dataframe = pd.DataFrame(dict_for_pandas)
-    pandas_dataframe.index = ticker_list
-    pandas_dataframe = pandas_dataframe.replace(to_replace='N/A', value=np.nan)
-
-    # Drop columns that consist of NaN's only
-    for item in pandas_dataframe:
-        if pandas_dataframe[item].count() == 0:
-            del pandas_dataframe[item]
+    pandas_dataframe = dops.make_pd_dataframe(api_dict, answer_list, param_list, ticker_list)
 
     # Convert columns to numeric if possible, handle exception and print column name otherwise
-    for colname in pandas_dataframe:
-        try:
-            pandas_dataframe[colname] = pd.to_numeric(pandas_dataframe[colname])
-        except ValueError:
-            print colname, 'could not be converted.'
 
-    # TODO Convert columns to datetimes if possible
-    # TODO Parse values with M, B for million/billion
 
     if write_to_csv:
         pandas_dataframe.to_csv(result_csv_path)
