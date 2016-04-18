492bf7f604fc0d3a5a055b7730269a790b3a41b1
Andrey Portnoy <aportnoy@ucsd.edu>
2016-04-17 19:02:57 -0700

    timeit dec: less newlines

--- yfc/_decorators.py
+++ yfc/_decorators.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 def timeit(func):
     """Timing decorator, prints out function name and execution time."""
     def timed(*args, **kwargs):
@@ -7,6 +8,6 @@ def timeit(func):
         result = func(*args, **kwargs)
         finish = datetime.now()
         diff = finish - start
-        print '\n', func.__name__, 'call took', diff.total_seconds(), 'seconds.\n'
+        print '\n', func.__name__, 'call took', diff.total_seconds(), 'seconds.'
         return result
     return timed
