3930b5b722dcdb1b58bfaae693a8d5f4f7278f27
Andrey Portnoy <aportnoy@ucsd.edu>
2016-04-18 01:45:37 -0700

    mult_historical added to __init__.py

--- yfc/__init__.py
+++ yfc/__init__.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from user_operations import current, historical, correlation_matrix
+from user_operations import current, historical, mult_historical, correlation_matrix
 
 __author__ = 'Andrey Portnoy'
 __title__ = 'yfc'
